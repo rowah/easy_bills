6434c2c9f118c93c7093c112a09af6e4cfb0a360
Rowa <jrowah@gmail.com>
2024-01-15 03:30:44 +0300

    Add security static analysis tool sobelow

--- mix.exs
+++ mix.exs
@@ -50,7 +50,8 @@ defmodule EasyBills.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:sobelow, "~> 0.13", only: [:dev, :test], runtime: false}
     ]
   end
 
