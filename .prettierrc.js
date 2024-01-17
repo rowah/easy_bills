4a175534a3004c1ebbdeaeff36ddeb830d2fdabd
Rowa <jrowah@gmail.com>
2024-01-17 11:56:45 +0300

    Fix failing code format

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+}
+
+// module.exports = {
+//   printWidth: 120,
+//   eexMultilineLineLength: 100,
+//   eexMultilineNoParens: ["link", "form_for"],
+// };
