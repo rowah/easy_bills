36ee2cbdfe333625f7e9dfc7a3c75e12a6d82955
Rowa <jrowah@gmail.com>
2024-04-22 22:39:11 +0300

    add error page for invalid or used confirmation token

--- lib/easy_bills_web/live/user_confirmation_live.ex
+++ lib/easy_bills_web/live/user_confirmation_live.ex
@@ -2,35 +2,43 @@ defmodule EasyBillsWeb.UserConfirmationLive do
   use EasyBillsWeb, :live_view
 
   alias EasyBills.Accounts
-  alias EasyBillsWeb.CommonComponents.Icons
+  alias EasyBillsWeb.CommonComponents.{ErrorPages, Icons}
 
   def render(%{live_action: :edit} = assigns) do
     ~H"""
-    <div class="mx-auto max-w-[600px] mt-20">
-      <div class="flex items-center">
-        <Icons.logo_icon_white />
-        <span class="ml-4 text-purple-600 font-bold text-3xl">Invoices</span>
-      </div>
-      <.header class="">Hi, <%= @user.username %></.header>
-      <p>Welcome to EasyBills,</p>
-      <hr class="my-2" />
-      <p class="mb-6">Please take a second to confirm <%= @user.email %> as your email address:</p>
-      <p>
-        Once you do, you will be able to opt-in to notifications of activity and access other features that require a valid email address.
-      </p>
-      <.simple_form for={@form} id="confirmation_form" phx-submit="confirm_account">
-        <.input field={@form[:token]} type="hidden" />
-        <:actions>
-          <.button phx-disable-with="Confirming..." class="w-1/2 mx-auto">
-            Confirm email
-          </.button>
-        </:actions>
-      </.simple_form>
-      <p class="mt-6">High fives, <br /> Team EasyBills</p>
+    <div>
+      <%= if assigns[:user] do %>
+        <div class="mx-auto max-w-[600px] mt-20">
+          <div class="flex items-center">
+            <Icons.logo_icon_white />
+            <span class="ml-4 text-purple-600 font-bold text-3xl">Invoices</span>
+          </div>
+          <.header class="">Hi, <%= @user.username %></.header>
+          <p>Welcome to EasyBills,</p>
+          <hr class="my-2" />
+          <p class="mb-6">
+            Please take a second to confirm <%= @user.email %> as your email address:
+          </p>
+          <p>
+            Once you do, you will be able to opt-in to notifications of activity and access other features that require a valid email address.
+          </p>
+          <.simple_form for={@form} id="confirmation_form" phx-submit="confirm_account">
+            <.input field={@form[:token]} type="hidden" />
+            <:actions>
+              <.button phx-disable-with="Confirming..." class="w-1/2 mx-auto">
+                Confirm email
+              </.button>
+            </:actions>
+          </.simple_form>
+          <p class="mt-6">High fives, <br /> Team EasyBills</p>
 
-      <p class="mt-4 text-gray-500 text-sm">
-        If the button above does not work, try copying and pasting the URL into your browser.
-      </p>
+          <p class="mt-4 text-gray-500 text-sm">
+            If the button above does not work, try copying and pasting the URL into your browser.
+          </p>
+        </div>
+      <% else %>
+        <ErrorPages.invalid_token_error_page />
+      <% end %>
     </div>
     """
   end
