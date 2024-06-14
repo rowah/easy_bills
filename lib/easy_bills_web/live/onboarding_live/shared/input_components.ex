cef5f3abfbe03ace6e049f67c4c6359980c4e820
Rowa <jrowah@gmail.com>
2024-06-14 13:01:58 +0300

    Turn placeholder text red when error are present

--- lib/easy_bills_web/live/onboarding_live/shared/input_components.ex
+++ lib/easy_bills_web/live/onboarding_live/shared/input_components.ex
@@ -96,7 +96,7 @@ defmodule EasyBillsWeb.OnboardingLive.Shared.InputComponents do
         id={@id}
         value={Phoenix.HTML.Form.normalize_value(@type, @value)}
         class={[
-          "mt-2 block w-full rounded-lg text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6 phx-no-feedback:border-zinc-300 phx-no-feedback:focus:border-zinc-400 border-zinc-300 focus:border-zinc-400",
+          "input-field mt-2 block w-full rounded-lg text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6 phx-no-feedback:border-zinc-300 phx-no-feedback:focus:border-zinc-400 border-zinc-300 focus:border-zinc-400",
           @errors == [] && "placeholder:text-gray-500",
           @errors != [] && "placeholder:text-red-500 text-error"
         ]}
