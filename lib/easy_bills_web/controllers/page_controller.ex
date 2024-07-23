4519150df2bbddf25eab3d64ed639356da8314ae
Rowa <jrowah@gmail.com>
2024-07-23 23:50:57 +0300

    delete redundant code

--- lib/easy_bills_web/controllers/page_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule EasyBillsWeb.PageController do
-  use EasyBillsWeb, :controller
-
-  def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home)
-  end
-end
